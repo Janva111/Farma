92f340756eaad77d05249d5003a1b36fea66e8a7
janva111 <vavraj08@gmail.com>
2025-01-30 10:55:55 +0100

    structure for animals

---
+++ src/Animals/Animal.java
@@ -0,0 +1,50 @@
+package Animals;
+
+public abstract class Animal {
+
+    protected String type;
+    protected String name;
+    protected Size size;
+    protected String specialCare;
+
+    public Animal(String type, String name, Size size, String specialCare) {
+        this.type = type;
+        this.name = name;
+        this.size = size;
+        this.specialCare = specialCare;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSpecialCare(String specialCare) {
+        this.specialCare = specialCare;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public String getSpecialCare() {
+        return specialCare;
+    }
+
+    @Override
+    public String toString() {
+        return "Animals.Animal{" +
+                "type='" + type + '\'' +
+                ", name='" + name + '\'' +
+                ", size=" + size +
+                ", specialCare='" + specialCare + '\'' +
+                '}';
+    }
+}
